5da876c481ee9989d8de85f33bd977396f76f105
Marcus Storm <storm.marcus@hotmail.com>
2023-10-02 19:50:01 +0200

    feat: add hyprland, kmonad laptop keyboard layout and sway monitor setup for home and office

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ ln -s ~/.dotfiles/.config/sway ~/.config
 ln -s ~/.dotfiles/.config/wofi ~/.config
 ln -s ~/.dotfiles/.config/waybar ~/.config
 ln -s ~/.dotfiles/.config/helix ~/.config
+ln -s ~/.dotfiles/.config/kmonad ~/.config
+ln -s ~/.dotfiles/.config/hypr ~/.config
