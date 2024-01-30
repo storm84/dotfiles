c77d4ef2022e0b8e8accda57f7a283a02364a4e2
storm84 <storm.marcus@hotmail.com>
2024-01-30 16:35:21 +0100

    feat add keymap for neo-tree git_status

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,5 +1,6 @@
 local neo_tree_config = function()
 	vim.keymap.set("n", "<leader>e", ":Neotree toggle filesystem reveal left<CR>", {})
+	vim.keymap.set("n", "<leader>ge", ":Neotree toggle float git_status<CR>", {})
 end
 
 return {
